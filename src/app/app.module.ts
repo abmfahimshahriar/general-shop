9d51d50383f3402c58e2b635529beaab16a11dc6
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-10-04 14:41:35 +0600

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -7,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [
+    ToolBarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports: [
+    ToolBarComponent
+  ]
+})
+export class CoreModule { }
