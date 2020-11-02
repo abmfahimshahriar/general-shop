b4749a3038bb1296b7574b65558fa3f7d4aba9c4
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-11-02 19:20:05 +0600

    add or remove cart functionality

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -9,23 +9,61 @@ import { Subscription } from 'rxjs';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   products: any[];
+  cart: any[] = [];
   subscription: Subscription;
   constructor(
     private productService: ProductService
-  ) { 
-    this.subscription = this.productService.getAllProducts().subscribe((data:any) => {
+  ) {
+    this.subscription = this.productService.getAllProducts().subscribe((data: any) => {
       this.products = data.products;
     })
   }
 
   ngOnInit(): void {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    if (cart) {
+      this.cart = cart;
+    }
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
   cartManager(cartItem) {
-    console.log(cartItem);
+    let num = 1;
+    if (cartItem.type === 'remove') {
+      num = -1;
+    }
+    let tempCart = [...this.cart];
+    let tempItem: any = tempCart
+      .find(item => item._id === cartItem.prodId);
+    console.log(tempItem);
+
+    if (!tempItem) {
+      if (cartItem.type !== 'remove') {
+        tempItem = this.products
+          .find(item => item._id === cartItem.prodId);
+        const total = tempItem.price;
+        const item = {
+          ...tempItem,
+          total,
+          count: 1
+        }
+        tempCart = [...tempCart, item];
+      }
+    }
+    else {
+      tempItem.count = tempItem.count + num;
+      if (tempItem.count === 0) {
+        tempCart = tempCart.filter(item => item._id !== tempItem._id);
+      }
+      else {
+        tempItem.total = tempItem.price * tempItem.count;
+        tempItem.total = parseFloat(tempItem.total.toFixed(2));
+      }
+    }
+    this.cart = [...tempCart];
+    localStorage.setItem('cart', JSON.stringify(this.cart));
   }
 
 }
--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -11,17 +11,28 @@ export class ProductCardComponent implements OnInit {
   @Input() product;
   @Output() addToCart = new EventEmitter<any>();
   baseUrl = baseUrl;
+  count = 0;
+  emptyCart;
   constructor() {
     
   }
 
   ngOnInit(): void {
+    const cart = localStorage.getItem('cart');
+    if(cart.length === 0) this.emptyCart = true;
+    else this.emptyCart = false;
     console.log(this.product);
     this.product.image = this.baseUrl + this.product.image;
   }
 
   onCart(prodId,type) {
-    console.log(prodId,type);
+    if(type === 'remove') {
+      if(this.count === 0) this.count = 0;
+      else this.count -= 1;
+    }
+    else{
+      this.count += 1
+    }
     const cartItem = {
       prodId: prodId,
       type: type
