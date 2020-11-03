48e176f391d953ba2678318a08a82c8ed47c1d93
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-11-03 13:44:30 +0600

    show items in cart menu

--- src/app/core/components/cart-menu/cart-menu.component.ts
+++ src/app/core/components/cart-menu/cart-menu.component.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartMenuComponent implements OnInit {
 
+  cart;
+  cartTotal;
   constructor() { }
 
   ngOnInit(): void {
+    this.cart = JSON.parse(localStorage.getItem('cart'));
+    const total = this.cart.map(item => item.total);
+    this.cartTotal = total.reduce((a, b) => a + b, 0);
   }
 
 }
