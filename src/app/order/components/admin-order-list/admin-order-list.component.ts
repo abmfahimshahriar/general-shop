4a66e80ec33fc8bf1cca6e12a7f3acfc119fa886
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-11-24 07:26:29 +0600

    order status change in progress

--- src/app/order/components/admin-order-list/admin-order-list.component.ts
+++ src/app/order/components/admin-order-list/admin-order-list.component.ts
@@ -34,4 +34,8 @@ export class AdminOrderListComponent implements OnInit {
     });
   }
 
+  changeOrderStatus(orderId) {
+    
+  }
+
 }
--- src/app/order/components/order-card/order-card.component.ts
+++ src/app/order/components/order-card/order-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-order-card',
@@ -7,10 +7,13 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class OrderCardComponent implements OnInit {
   @Input() orderDetails;
-
+  @Output() changeStatus = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  chageOrderStatus(orderId) {
+    this.changeStatus.emit(orderId)
+  }
 }
