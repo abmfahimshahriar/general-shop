59bfc61bba9c4257e09ff0ca2e466ea03e1033ac
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-23 08:26:25 +0600

    add authentication in sidenav

--- src/app/core/components/side-nav/side-nav.component.ts
+++ src/app/core/components/side-nav/side-nav.component.ts
@@ -1,21 +1,31 @@
 import { SideNavService } from './../../services/side-nav.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from 'src/app/store/store';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
   @ViewChild('drawer') public sideNav: MatSidenav;
   showFiller = false;
 
-  constructor(private sideNavService: SideNavService) { 
+  authenticated: boolean;
+  authenticatedAsAdmin: boolean;
+  constructor(
+    private sideNavService: SideNavService,
+    private ngRedux: NgRedux<IAppState>
+  ) {
+    this.ngRedux.subscribe(() => {
+      this.authenticated = this.ngRedux.getState().authenticated;
+      this.authenticatedAsAdmin = this.ngRedux.getState().authenticatedAsAdmin;
+    });
   }
 
-  ngOnInit() { 
-  }
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     this.sideNavService.setSidenav(this.sideNav);
@@ -24,6 +34,4 @@ export class SideNavComponent implements OnInit {
   onClickSideNavItem() {
     this.sideNavService.close();
   }
-   
-
 }
